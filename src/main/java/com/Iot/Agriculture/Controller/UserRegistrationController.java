1943780a6f4f6ae2f06c76ba7cf6ff895d5317e9
ravindra1111 <54111248+ravindra1111@users.noreply.github.com>
2021-09-14 11:47:44 +0530

    +Refactored all controllers. +proper validation has been implemented.
    1.Need to add some functionality refer Controller-Readme.md
    
    Removed precip data variable.

--- src/main/java/com/Iot/Agriculture/Controller/UserRegistrationController.java
+++ src/main/java/com/Iot/Agriculture/Controller/UserRegistrationController.java
@@ -14,6 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/*
+ * *****************************
+ *  By: Ravindranadh Inavolu
+ * *****************************
+ * */
+
 @Controller
 @RestController
 public class UserRegistrationController {
